829eccba981d2bdcd4a2c64486f79a5ff055cf4e
Charif Abdallah <charifabdallah@iut-dhaka.edu>
2023-02-04 01:26:40 +0600

    flowershop

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// var menu = document.querySelector('#menu-bar');
+// var navbar = document.querySelector('.navbar');
+
+
+// window.onscroll = function() {
+//     searchBtn.classList.remove('fa-times');
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// };
+
+// menu.addEventListener('click', function() {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// });
